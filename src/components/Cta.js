edf2fccf6e5a98d46ba783359be68985f7a8cd6b
mate <matemislov90@gmail.com>
2020-02-10 20:00:12 +0100

    Apply

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -3,8 +3,8 @@ import { Link } from "gatsby"
 
 export default function Cta({ className, amount = 2000, term = 12 }) {
   return (
-    <Link
-      to="/apply/"
+    <a
+      href="/apply/"
       state={{ amount, term }}
       className={
         "text-white inline-flex items-center justify-center bg-brand-blue  inline-block tracking-wide  p-2 w-48 h-12 rounded-full " +
@@ -13,6 +13,6 @@ export default function Cta({ className, amount = 2000, term = 12 }) {
     >
       {" "}
       Get Started
-    </Link>
+    </a>
   )
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,7 @@ import Cta from "./Cta"
 import CookieConsent from "react-cookie-consent"
 import { useStaticQuery, graphql } from "gatsby"
 import css from "@emotion/css"
-export default function Layout({ children }) {
+export default function Layout({ children, formPage }) {
   const [menuIsOpen, setMenuIsOpen] = React.useState(false)
   const { cookies } = useStaticQuery(graphql`
     {
@@ -27,31 +27,37 @@ export default function Layout({ children }) {
       <SideMenu {...{ menuIsOpen, setMenuIsOpen }}></SideMenu>
       <Header {...{ menuIsOpen, setMenuIsOpen }}></Header>
       {children}
-      <div className="container text-center pb-16 -mt-8">
-        <Cta className="w-64 h-16 px-12" />
-      </div>
-      <Reviews></Reviews>
-      <Footer></Footer>
-      <CookieConsent
-        style={{ alignItems: "center" }}
-        buttonText="Accept"
-        buttonStyle={{ color: "#000", background: "#fff" }}
-        enableDeclineButton={false}
-        flipButtons
-      >
-        <div
-          css={css`
-            a {
-              text-decoration: underline;
-              color: #fff;
-            }
-            p {
-              margin-bottom: 0;
-            }
-          `}
-          dangerouslySetInnerHTML={{ __html: cookies && cookies.text.md.html }}
-        ></div>
-      </CookieConsent>
+      {!formPage && (
+        <>
+          <div className="container text-center pb-16 -mt-8">
+            <Cta className="w-64 h-16 px-12" />
+          </div>
+          <Reviews></Reviews>
+          <Footer></Footer>
+          <CookieConsent
+            style={{ alignItems: "center" }}
+            buttonText="Accept"
+            buttonStyle={{ color: "#000", background: "#fff" }}
+            enableDeclineButton={false}
+            flipButtons
+          >
+            <div
+              css={css`
+                a {
+                  text-decoration: underline;
+                  color: #fff;
+                }
+                p {
+                  margin-bottom: 0;
+                }
+              `}
+              dangerouslySetInnerHTML={{
+                __html: cookies && cookies.text.md.html,
+              }}
+            ></div>
+          </CookieConsent>
+        </>
+      )}
     </div>
   )
 }
